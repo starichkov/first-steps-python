bd691078494156df601d88f9def3b2a8bb4b1b40
Vadim Starichkov <starichkovva@gmail.com>
2016-05-06 23:26:42 +0400

    Tuple unpacking

--- collections/tuples.py
+++ collections/tuples.py
@@ -13,3 +13,18 @@ print(words1)
 
 empty_words = ()
 print(empty_words)
+
+# tuple unpacking
+print("Tuple unpacking:")
+numbers = (1, 2, 3)
+a, b, c = numbers
+print(a)
+print(b)
+print(c)
+
+print("Tuple unpacking:")
+a, b, *c, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+print(b)
+print(c)
+print(d)
